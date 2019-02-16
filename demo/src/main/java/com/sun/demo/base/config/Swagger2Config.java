52caadbe0c2cc19e4e3b3e3fcbe26c82eb93c35c
szy992 <295508987@qq.com>
2019-02-16 20:56:55 +0800

    添加swagger2接口文档功能
    添加swagger2接口文档功能

---
+++ demo/src/main/java/com/sun/demo/base/config/Swagger2Config.java
@@ -0,0 +1,134 @@
+package com.sun.demo.base.config;
+
+import com.google.common.collect.Sets;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+@ConfigurationProperties(prefix="swagger2")
+public class Swagger2Config {
+	private String userName;
+	private String email;
+	private String url;
+	private String title;
+	private String desc;
+	private String version;
+	private String license;
+	private String licenseUrl;
+	private String termsOfServiceUrl;
+	private String basePackage;
+
+	@Bean
+	public Docket getDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).
+				apiInfo(getApi()).
+				select().
+				apis(RequestHandlerSelectors.basePackage(basePackage)).build();
+	}
+	// 如何把对象放在容器
+	// 1 配置<bean> 2 通过注解 Companent Service Resp 
+	// 在方法上面，通过Bean 里注入容器
+
+	private ApiInfo getApi() {
+		Contact contact = new Contact(userName, url, email);
+		return new ApiInfoBuilder().
+				title(title).
+				description(desc).
+				license(license).
+				licenseUrl(licenseUrl).
+				termsOfServiceUrl(termsOfServiceUrl).
+				version(version).
+				contact(contact).build();
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getLicense() {
+		return license;
+	}
+
+	public void setLicense(String license) {
+		this.license = license;
+	}
+
+	public String getLicenseUrl() {
+		return licenseUrl;
+	}
+
+	public void setLicenseUrl(String licenseUrl) {
+		this.licenseUrl = licenseUrl;
+	}
+
+	public String getTermsOfServiceUrl() {
+		return termsOfServiceUrl;
+	}
+
+	public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+		this.termsOfServiceUrl = termsOfServiceUrl;
+	}
+
+	public String getBasePackage() {
+		return basePackage;
+	}
+
+	public void setBasePackage(String basePackage) {
+		this.basePackage = basePackage;
+	}
+	
+}
--- demo/src/main/java/com/sun/demo/controller/HelloWorld.java
+++ demo/src/main/java/com/sun/demo/controller/HelloWorld.java
@@ -2,11 +2,23 @@ package com.sun.demo.controller;
 
 import com.sun.demo.dto.HUSER;
 import com.sun.demo.service.HelloService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api(value = "测试接口1", tags = "测试接口", description = "Hello World")
+@ApiResponses(value = {
+        @ApiResponse(code = 200, message = "请求正常完成"),
+        @ApiResponse(code = 400, message = "请求中有语法问题，或不能满足请求"),
+//        @ApiResponse(code = 401, message = "未授权客户机访问数据"),
+//        @ApiResponse(code = 403, message = "服务器接受请求，但是拒绝处理"),
+//        @ApiResponse(code = 404, message = "服务器找不到给定的资源，文档不存在"),
+        @ApiResponse(code = 500, message = "服务器出现异常")}
+)
 @RestController
 public class HelloWorld {
 
@@ -25,7 +37,7 @@ public class HelloWorld {
     @Autowired
     public HelloService helloService;
 
-
+    @ApiOperation(value = "hello", notes = "hello", produces = "application/json")
     @GetMapping(value = "hello")
     public String Hello(){
         helloService.say();
@@ -33,7 +45,7 @@ public class HelloWorld {
         return "hello world";
     }
 
-
+    @ApiOperation(value = "检查用户信息", notes = "检查用户信息", produces = "application/json")
     @GetMapping(value = "select")
     public HUSER hello(){
         HUSER huser = helloService.select(new Long(1));
